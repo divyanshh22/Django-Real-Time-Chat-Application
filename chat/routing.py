0b645a88244a1e566ad4ea6449323451f89cd73b
Divyansh Singh <129421803+divyanshh22@users.noreply.github.com>
2026-07-31 18:45:41 +0530

    Update routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,8 @@
+from django.urls import path
+from .consumers import ChatConsumer
 from .presence_consumer import PresenceConsumer
 
 websocket_urlpatterns = [
-    path("ws/chat/<str:username>/", ChatConsumer.as_asgi()),   # ye already hoga
-    path("ws/presence/", PresenceConsumer.as_asgi()),           # ye naya add karo
+    path("ws/chat/<str:username>/", ChatConsumer.as_asgi()),
+    path("ws/presence/", PresenceConsumer.as_asgi()),
 ]
